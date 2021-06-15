b61f83621daa40d5824ced8581d6334403343ecc
James Danielson <geekylink@geekylink.github.io>
2021-06-15 00:53:40 -0400

    Path sum, python solution

---
+++ leet-code/Algorithms/112-path_sum/sol_a.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        
+        if root.left:
+            res = self.hasPathSum(root.left, sum - root.val)
+            if res:
+                return True
+        
+        if root.right:
+            res = self.hasPathSum(root.right, sum - root.val)
+            if res:
+                return True
+        
+        if not root.left and not root.right and sum == root.val:
+            return True
+        
+        return False
